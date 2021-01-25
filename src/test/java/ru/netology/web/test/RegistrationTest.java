d172b4363d99df63322bee1d23a96a8e034d6ac1
melomanos <philippov_mihail@mail.ru>
2021-01-25 14:07:03 +0300

    Allure added

--- src/test/java/ru/netology/web/test/RegistrationTest.java
+++ src/test/java/ru/netology/web/test/RegistrationTest.java
@@ -1,6 +1,10 @@
 package ru.netology.web.test;
 
 import com.codeborne.selenide.conditions.Text;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -17,6 +21,16 @@ class RegistrationTest {
     @BeforeEach
     void setUp() {
         open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
